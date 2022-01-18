3a3c31b98c74efbf1b7a4a3a80a9750d36c43ce0
richarbsc <r.v13_98@hotmail.com>
2022-01-18 15:36:32 -0500

    Inicio del Proyecto SGC

---
+++ SistemaGestionConcurso/Modelo/Entidades/Configuracion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.Entidades
+{
+    public class Configuracion
+    {
+        public float NotaMinima { get; set; }
+        public float PesoNota1 { get; set; }
+        public float PesoNota2 { get; set; }
+
+
+        //Relacion con Concurso 
+        public Concurso ConcursoVigente { get; set; }
+
+
+    }
+}
---
+++ SistemaGestionConcurso/Modelo/Entidades/Participante.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.Entidades
+{
+    public class Participante
+    {
+        public int ParticipanteId { get; set; } 
+        public string NombreParticipante { get; set; }
+        public int Cedula { get; set; }
+        public string Direccion { get; set; }
+        public int Telefono { get; set; }
+        public int Celular { get; set; }
+        public string Correo { get; set; }
+
+    }
+}
