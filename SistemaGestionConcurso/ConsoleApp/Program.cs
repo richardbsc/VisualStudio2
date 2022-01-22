89f5f8a01234b325d9c0be599c7db5b0256e3e8c
richarbsc <r.v13_98@hotmail.com>
2022-01-21 22:18:08 -0500

    actualizacion 1

---
+++ SistemaGestionConcurso/ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using Modelo.Entidades;
+using ModeloBD;
+using System;
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main (string[] args)
+        {
+            Participante participante = new Participante(); { };
+            Configuracion Config = new Configuracion();
+            {
+                Nota = 7.0f
+
+            }
+            Repositorio repos;
+
+        }
+    }
+}
---
+++ SistemaGestionConcurso/ModeloBD/Repositorio.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Modelo.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeloBD
+{
+    public class Repositorio: DbContext
+    {
+        //Configuracion de las entidades
+        public DbSet<Participante> Participantes { get; set;}
+        public DbSet<Calificacion> Calificaciones { get; set;}
+    }
+}
